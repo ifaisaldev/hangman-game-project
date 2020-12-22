81245fe320761fbec4f494f22c9fba53bc331b01
Faisal Mehmood <fasiawan6401@gmail.com>
2020-12-22 03:50:57 -0800

    Javascript has been done to display words.

---
+++ js/style.js
@@ -0,0 +1,15 @@
+const wordEl = document.getElementById('word');
+const wordLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-again');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+const figureParts = document.querySelectorAll('.figure-part');
+const words = ['application', 'programming', 'interface', 'wizard'];
+let selectedWord = words[Math.floor(math.random() * words.length)];
+const correctLetters = [];
+const wrongLetters = [];
+function displayWord(){
+    wordEl.innerHTML = `${selectedWord.split('').map(letter => `<span class="letter">${correctLetters.includes(letter) ? letter : ''}</span>`)}`;
+}
+displayWord();
